01d718cbce4e0d70d8d32e5b532a602993ce9017
Jake <jakedves@gmail.com>
2021-08-25 00:47:38 +0100

    Landing page complete

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+
+export default function Home() {
+    const titles = ["iOS Developer", "Java Developer", "Web Developer", "Computer Scientist"]
+    const [title, setTitle] = useState(0)
+
+    return (
+        <div className="home">
+            <div className="home-content">
+                <h1>Jake Davies</h1>
+            
+                <div className="title-box">
+                    <h3>{titles[title % titles.length]}</h3>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
