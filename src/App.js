7605250594e0ebef8f0871894d0df6ad74f3efd9
Jake <jakedves@gmail.com>
2021-10-17 23:04:12 +0100

    Added projects page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Home from "./Home.js";
 import About from "./About.js";
+import Projects from "./Projects";
 
 function App() {
   // pages should include:
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Home />
       <About />
+      <Projects />
     </div>
   );
 }
---
+++ src/Projects.js
@@ -0,0 +1,7 @@
+export default function Projects() {
+  return (
+    <div className="projects">
+      <h1 className="projects-title">Take a look.</h1>
+    </div>
+  );
+}
