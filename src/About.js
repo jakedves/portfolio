fde6acdd209d6f133ae2163bb021119495f2784f
Jake <jakedves@gmail.com>
2021-10-17 22:43:33 +0100

    Updated about page with text and images

--- src/About.js
+++ src/About.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import AboutCard from "./AboutCard.js";
+import AboutText from "./AboutText.js";
+import MKW from "./MarioKartWii.png";
+import SquidGame from "./SquidGame.jpg";
 
 export default function About() {
   return (
     <div className="about">
-      <AboutCard title="Playing" />
-      <AboutCard title="Current Project" />
-      <AboutCard title="Watching" />
-      <AboutCard title="Studying" />
+      <AboutText
+        title="Hi, I'm Jake."
+        body="I love experimenting with technology and trying new things."
+      />
+      <div className="about-card-container">
+        {/* Mario Kart Wii, Health, AoT, STUDYING */}
+        <AboutCard title="Playing" imagePath={MKW} />
+        <AboutCard title="Working On" description="my sigma mindset" />
+        <AboutCard title="Watching" imagePath={SquidGame} />
+        <AboutCard title="Studying" />
+      </div>
     </div>
   );
 }
