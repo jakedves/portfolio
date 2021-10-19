f57dfa931abb47cc60ba5ce37d0ba7fbd7e1afff
Jake <jakedves@gmail.com>
2021-10-20 00:08:19 +0100

    Created some project objects

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,23 @@
 import Showcase from "./Showcase";
 
 export default function Projects() {
-  let projects = ["Project 1", "Project 2", "Project 3"];
+  let projects = [
+    {
+      name: "Recipe Builder for iOS",
+      desc: "The ultimate recipe building app for iOS",
+      image: "",
+    },
+    {
+      name: "The JDX Compiler",
+      desc: "A compiler written in Swift",
+      image: "",
+    },
+    {
+      name: "Sleep & Mood Tracker for Desktop",
+      desc: "A Java app that tracks a users mood and sleep over time, giving them tips on how to improve their lifestyle",
+      image: "",
+    },
+  ];
 
   return (
     <div className="projects">
