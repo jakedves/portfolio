a4245c6b9ed5c7741a299f82f2f7fd42046f7ac8
Jake <jakedves@gmail.com>
2021-10-22 11:11:27 +0100

    Added optional image support for projects section
    Projects objects now can also have a 'tools' attribute where I can list the tools/techniques i used for that project

--- src/Projects.js
+++ src/Projects.js
@@ -1,21 +1,38 @@
 import Showcase from "./Showcase";
+import RecipeBuilder from "./RecipeBuilderiOS.png";
 
 export default function Projects() {
   let projects = [
     {
       name: "Recipe Builder for iOS",
-      desc: "The ultimate recipe building app for iOS",
-      image: "",
+      desc: `Recipe Builder is the ultimate recipe-building app, made for iOS. It is simple. Fast. Easy to use.
+       Recipe Builder is an app that cuts out all the details of other recipe-building apps. No need to enter the cost
+       of a recipe, how many calories it has, or anything else you don't care about. Aimed at students, Recipe Builder is 
+       the most convenient solution available. Download on the App Store today.`,
+      image: RecipeBuilder,
+      tools: ["Swift", "SwiftUI", "UIKit", "Core Data", "CloudKit"],
     },
     {
       name: "The JDX Compiler",
       desc: "A compiler written in Swift",
-      image: "",
     },
     {
       name: "Sleep & Mood Tracker for Desktop",
-      desc: "A Java app that tracks a users mood and sleep over time, giving them tips on how to improve their lifestyle",
-      image: "",
+      desc: `A Java app that tracks a users mood and sleep over time, giving them tips on how to 
+      improve their lifestyle`,
+      tools: ["Java", "Java Swing", "SQL", "JFreeChart", "Scrum Methodology"],
+    },
+    {
+      name: "Worlds Apart",
+      desc: `This game was a submission for the GMTK 2021 Game Jam, where we had 48 hours to design,
+       develop and submit a game, based on the theme: Joined Together. The basis of the game is that you are 
+       alone, lost in the world and you need to solve some puzzles to escape. Luckily for you, what you realise 
+       is that you are actually in two places, at once (or are you?), and you have to work with yourself on one side, 
+       to unlock puzzles on the other side.
+       \n
+       This game was made using Unity3D, C# and we worked with pair programming, and designed the map ourselves. The assets
+        used in the game were all free assets from the Unity Asset Store.`,
+      tools: ["C#", "Unity 3D", "Unity Animator", "Scripting"],
     },
   ];
 
--- src/ShowcaseItem.js
+++ src/ShowcaseItem.js
@@ -1,11 +1,13 @@
 export default function ShowcaseItem({ project, rightAligned }) {
   return (
     <div
-      className={
-        "showcase-item " + `${rightAligned ? "right-aligned" : "left-aligned"}`
-      }
+      className={`showcase-item ${
+        rightAligned ? "right-aligned" : "left-aligned"
+      }`}
     >
-      <img src={project.image} alt="" />
+      {project.image ? (
+        <img src={project.image} alt={`${project.name}`} />
+      ) : null}
       <div className="showcase-text">
         {/* attributes here are empty */}
         <strong>{project.name}</strong>
