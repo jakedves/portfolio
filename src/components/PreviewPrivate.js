b9bdabfaaee1eb6743772360570eec6ec175a5d6
Jake <jakedves@gmail.com>
2021-11-26 01:36:34 +0000

    Added teaser for private code

---
+++ src/components/PreviewPrivate.js
@@ -0,0 +1,34 @@
+/**
+ * This component will be used to tease the private repositories I have
+ * on GitHub. Below this will come the contact me section so that I can
+ * be contacted.
+ */
+export default function PreviewPrivate() {
+  let images = [
+    {
+      path: "",
+      caption: "",
+    },
+  ];
+  let teaserTitle = "Want to see some more?";
+  let teaserText = `I have numerous, fully documented University projects that I can't make public for plagiarism reasons,
+  however, I can still show off the code so don't hesitate to ask to see it!`;
+
+  return (
+    <section className="private-projects">
+      <div className="image-row">
+        {images.map((image) => {
+          return (
+            <img
+              src={Image.path}
+              caption={image.caption}
+              alt={image.caption}
+            ></img>
+          );
+        })}
+      </div>
+      <h1 className="section-title">{teaserTitle}</h1>
+      <h5 className="teaser-text">{teaserText}</h5>
+    </section>
+  );
+}
