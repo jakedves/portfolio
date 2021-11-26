a0e4952a31de276a8d742de7591ade8fc74997a5
Jake <jakedves@gmail.com>
2021-11-26 01:36:24 +0000

    Begun Contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,21 @@
+/**
+ * This component will be used to provide people with a way to contact me,
+ * so they can ask me questions, or ask to see some of my other work.
+ * @returns : The contact form
+ */
 export default function Contact() {
   return (
     <div>
       <form>
-        <input>Name</input>
-        <input>email</input>
-        <input>message</input>
-        <button>submit</button>
+        <input type="text" id="name" name="name" placeholder="Name"></input>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email Address"
+        ></input>
+        <textarea id="subject" name="subject" placeholder="Message"></textarea>
+        <input type="submit" value="Submit"></input>
       </form>
     </div>
   );
